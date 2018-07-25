93a45708e7233474b61ebb380b0f35107c8c8857
jianhui.jiang <jianhui.jiang@phicomm.com>
2018-07-25 09:11:53 +0800

    conflict fixed

--- Main.java
+++ Main.java
@@ -1,12 +1,8 @@
 public class Main{
     public static void main(String...args){
         System.out.println("Hello Git!");
-<<<<<<< HEAD
 	int x = 10;
 	System.out.println(x);
-=======
 	String str = "I'm very happy";
-
->>>>>>> fun1
     }
 }
