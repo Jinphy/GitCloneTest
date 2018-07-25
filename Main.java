1d9ac02e62741dbb03edddef85f62bcd25699646
jianhui.jiang <jianhui.jiang@phicomm.com>
2018-07-25 09:18:48 +0800

    Delete int x

--- Main.java
+++ Main.java
@@ -1,8 +1,6 @@
 public class Main{
     public static void main(String...args){
         System.out.println("Hello Git!");
-	int x = 10;
-	System.out.println(x);
 	String str = "I'm very happy";
     }
 }
